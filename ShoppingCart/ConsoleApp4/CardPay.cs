c6ddc56ea3592d846f475101a04d2fc497cfb07e
Shrijeet Karade <skarade@tavisca.com>
2018-09-05 12:28:45 +0530

    Shopping Cart

---
+++ ShoppingCart/ConsoleApp4/CardPay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class CardPayment:Cart,IPaymentDetails
+    {
+        void SetDetails()
+        {
+
+        }
+
+        void IPaymentDetails.SetDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pay()
+        {
+            Console.WriteLine(  "payment success through card");
+        }
+    }
+}
