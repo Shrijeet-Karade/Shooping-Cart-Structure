0fa77845d2124fc04c4dbca9355ab1f937c9eeff
Shrijeet Karade <skarade@tavisca.com>
2018-09-05 14:18:39 +0530

    factory Structure

---
+++ FactoryStructure/ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+   public class Program:SelectType
+    {
+       public static void Main(string[] args)
+        {
+            string desc = Console.ReadLine();
+            IProduct prod = GetProduct(desc);
+            prod.GetTypeofProduct();
+            prod.Book();
+            prod.Save();
+        }
+
+        private static IProduct GetProduct(string desc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class SelectType
+    {
+        private IProduct GetProduct(string desc)
+        {
+
+            return new CarProduct();
+        }
+    }
+}
