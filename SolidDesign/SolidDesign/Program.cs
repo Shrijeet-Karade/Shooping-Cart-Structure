dbdf0f3b0b8df610658fe3280b1c2bd00fd2dc5d
Shrijeet Karade <skarade@tavisca.com>
2018-09-06 10:54:50 +0530

    Version 2(Need to make some Changes)

---
+++ SolidDesign/SolidDesign/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cart cartObj = new Cart();
+            cartObj.itemId = 1;
+            cartObj.Item = new List<ItemDetails>();
+            Inventory inventoryObj = new Inventory();
+            FunctionsInsideCart cartOperationObject = new FunctionsInsideCart();
+            List<ItemDetails> availableItems = inventoryObj.GetAllItems();
+        }
+    }
+}
